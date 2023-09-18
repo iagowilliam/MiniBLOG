afb99dd0a7f10dca2df00d7302622bb5b2653151
Iago William Bastos de Souza <iagowilliam@hotmail.com>
2023-09-18 13:07:33 -0300

    Criado o formulário de cadastro

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,15 @@ import './App.css'
 
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 
+// components
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+
 // pages
 import Home from './pages/Home/Home'
 import About from './pages/About/About'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
+import Login from './pages/Login/Login'
+import Register from './pages/Register/Register'
 
 function App() {
 
@@ -21,6 +25,8 @@ function App() {
             <Routes>
               <Route path='/' element={<Home />}/>
               <Route path='/about' element={<About />}/>
+              <Route path='/login' element={<Login />}/>
+              <Route path='/register' element={<Register />}/>
             </Routes>
           </div>
 
