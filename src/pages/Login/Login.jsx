fb53f0b66ee87d2e2f7e597164c739c6882b00a0
Iago William Bastos de Souza <iagowilliam@hotmail.com>
2023-09-20 16:43:09 -0300

    Feito a estruturação da página login e os seus estilos

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,59 @@
 import styles from "./Login.module.css"
 
+import { useState, useEffect } from "react"
+
+import { useAuthentication } from "../../hooks/useAuthentication"
+
 const Login = () => {
+
+    const [email, setEmail] = useState("")
+    const [password, setPassoword] = useState("")
+    const [error, setError] = useState("")
+
+    const {createUser, error: authError, loading} = useAuthentication()
+
+    const handleLogin = async (e) => {
+        e.preventDefault()
+
+        setError("")
+
+        const user = {
+            email,
+            password,
+        }
+
+        const res = await createUser(user)
+
+        console.log(res)
+    }
+
+    useEffect(() => {
+
+        if(authError){
+            setError(authError)
+        }
+
+    },[authError])
+
   return (
-    <div>
-        <h2>Login</h2>
+    <div className={styles.login}>
+        <h1>Entrar</h1>
+            <p>Faça o login para poder utilizar o sistema</p>
+            <form onSubmit={handleLogin}>
+                <label>
+                    <span>Email:</span>
+                    <input type="email" name="email" required placeholder="Email do usuário" value={email}
+                        onChange={(e) => setEmail(e.target.value)} />
+                </label>
+                <label>
+                    <span>Senha:</span>
+                    <input type="password" name="password" required placeholder="Insira a sua senha" value={password}
+                        onChange={(e) => setPassoword(e.target.value)} />
+                </label>
+                {!loading && <button className="btn">Entrar</button>}
+                {loading && <button className="btn" disabled>Aguarde...</button>}
+                {error && <p className="error">{error}</p>}
+            </form>
     </div>
   )
 }
