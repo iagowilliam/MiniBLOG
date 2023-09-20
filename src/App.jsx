f03dd6cd9a244b674e3345f84cc5816c52d210ec
Iago William Bastos de Souza <iagowilliam@hotmail.com>
2023-09-20 16:21:02 -0300

    Feito o bloqueamento das paginas que precisam de autenticação e a estilzação da página sobre

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ import { AuthProvider } from './context/AuthContext'
 function App() {
 
   const [user, setUser] = useState(undefined)
-  const {auth} = useAuthentication()
+  const { auth } = useAuthentication()
 
   const loadingUser = user === undefined
 
@@ -36,16 +36,16 @@ function App() {
       setUser(user)
     })
 
-  },[auth])
+  }, [auth])
 
-  if(loadingUser){
+  if (loadingUser) {
     return <p>Carregando...</p>
   }
 
   return (
     <>
       <div className='App'>
-        <AuthProvider value={{user}}>
+        <AuthProvider value={{ user }}>
           <BrowserRouter>
 
             <Navbar />
@@ -54,10 +54,10 @@ function App() {
               <Routes>
                 <Route path='/' element={<Home />} />
                 <Route path='/about' element={<About />} />
-                <Route path='/login' element={<Login />} />
-                <Route path='/register' element={<Register />} />
-                <Route path='/posts/create' element={<CreatePost />} />
-                <Route path='/dashboard' element={<Dashboard />} />
+                <Route path='/login' element={!user ? <Login /> : <Navigate to="/" />} />
+                <Route path='/register' element={!user ? <Register /> : <Navigate to="/" />} />
+                <Route path='/posts/create' element={user ? <CreatePost /> : <Navigate to="/login" />} />
+                <Route path='/dashboard' element={user ? <Dashboard /> : <Navigate to="/login" />} />
               </Routes>
             </div>
 
