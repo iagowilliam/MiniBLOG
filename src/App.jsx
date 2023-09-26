ccd1e1b8c9cbbe7c656d0f84b8f5e1c73a410091
Iago William Bastos de Souza <iagowilliam@hotmail.com>
2023-09-26 17:09:12 -0300

    Feita a criação da página individual do post

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,7 @@ import Register from './pages/Register/Register'
 import Dashboard from './pages/Dashboard/Dashboard'
 import CreatePost from './pages/CreatePost/CreatePost'
 import Search from './pages/Search/Search'
+import Post from './pages/Post/Post'
 
 // context
 import { AuthProvider } from './context/AuthContext'
@@ -55,6 +56,7 @@ function App() {
                 <Route path='/' element={<Home />} />
                 <Route path='/about' element={<About />} />
                 <Route path='/search' element={<Search />} />
+                <Route path='/posts/:id' element={<Post />} />
                 <Route path='/login' element={!user ? <Login /> : <Navigate to="/" />} />
                 <Route path='/register' element={!user ? <Register /> : <Navigate to="/" />} />
                 <Route path='/posts/create' element={user ? <CreatePost /> : <Navigate to="/login" />} />
